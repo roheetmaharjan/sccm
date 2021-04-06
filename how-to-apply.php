fd07c587a74b88c87b186e4a794d4e6719663caa
rmaharjan <rmaharjan@innoviacloud.com>
2021-04-06 10:12:16 +0545

    form slide in and chose file option design  fix

--- how-to-apply.php
+++ how-to-apply.php
@@ -28,44 +28,50 @@ include('include/header.php'); ?>
                     <option>International students</option>
                 </select>
             </div>
-            <div class="how-to-steps">
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 1</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/apply-for-course.png" alt=""></figure>
-                    <h3 class="how-to-step-title">Apply for course</h3>
-                    <p class="how-to-step-description">Select courses from the list of courses you can fill out the application form manually by downloading the form and email to us at admissions@sccm.edu.au or fill out the online application form.</p>
+            <div class="how-to-steps-wrp">
+                <div class="how-to-steps">
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 1</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/apply-for-course.png" alt=""></figure>
+                        <h3 class="how-to-step-title">Apply for course</h3>
+                        <p class="how-to-step-description">Select courses from the list of courses you can fill out the application form manually by downloading the form and email to us at admissions@sccm.edu.au or fill out the online application form.</p>
+                    </div>
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 2</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/assessment-process.png" alt=""></figure>
+                        <h3 class="how-to-step-title">Assessment Process </h3>
+                        <p class="how-to-step-description">The admission team will assess the documents that you or your agency has submitted on your behalf. Based on the assessment outcome. At this stage you may require to appear for an interview (online or telephonic). The admission may issue an offer letter (Conditional or unconditional). To receive an unconditional letter require student will require to fulfill the condition and based on the document verification, Letter of offer is provided.</p>
+                    </div>
                 </div>
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 2</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/assessment-process.png" alt=""></figure>
-                    <h3 class="how-to-step-title">Assessment Process </h3>
-                    <p class="how-to-step-description">The admission team will assess the documents that you or your agency has submitted on your behalf. Based on the assessment outcome. At this stage you may require to appear for an interview (online or telephonic). The admission may issue an offer letter (Conditional or unconditional). To receive an unconditional letter require student will require to fulfill the condition and based on the document verification, Letter of offer is provided.</p>
+                <div class="how-to-steps">
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 3</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/ecoe.png" alt=""></figure>
+                        <h3 class="how-to-step-title">eCOE (Electronic confirmation of enrolment)</h3>
+                        <p class="how-to-step-description">When the payment is received, the college will issue the Ecoe</p>
+                    </div>
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 4</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/offer-letter.png" alt=""></figure>
+                        <h3 class="how-to-step-title">Offer letter </h3>
+                        <p class="how-to-step-description">The student is required to review offer letter and read through terms and conditions, tuition fees and all other important information. Student will need to accept the offer and affix their signature. The signed offer letter along with payment of deposit will need to revert to admissions team.</p>
+                    </div>
                 </div>
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 3</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/ecoe.png" alt=""></figure>
-                    <h3 class="how-to-step-title">eCOE (Electronic confirmation of enrolment)</h3>
-                    <p class="how-to-step-description">When the payment is received, the college will issue the Ecoe</p>
-                </div>
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 4</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/offer-letter.png" alt=""></figure>
-                    <h3 class="how-to-step-title">Offer letter </h3>
-                    <p class="how-to-step-description">The student is required to review offer letter and read through terms and conditions, tuition fees and all other important information. Student will need to accept the offer and affix their signature. The signed offer letter along with payment of deposit will need to revert to admissions team.</p>
-                </div>
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 5</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/student-visa.png" alt=""></figure>
-                    <h3 class="how-to-step-title">Student Visa </h3>
-                    <p class="how-to-step-description">The student will need to apply for student visa from the closed Australian Missions overseas. More information can be option from
-                        https://immi.homeaffairs.gov.au/</p>
-                </div>
-                <div class="how-to-step">
-                    <h4 class="step-number">Step 6</h4>
-                    <figure class="how-to-step-img"><img src="./assets/img/icon/departure-and-orientation.png" alt=""></figure>
-                    <h3 class="how-to-step-title">Departure and Orientation </h3>
-                    <p class="how-to-step-description">Once the student receives the visa they will need to plan their departure for Australia. Upon arrival, student will join the orientation session get officially enrolled into the program</p>
+                <div class="how-to-steps">
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 5</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/student-visa.png" alt=""></figure>
+                        <h3 class="how-to-step-title">Student Visa </h3>
+                        <p class="how-to-step-description">The student will need to apply for student visa from the closed Australian Missions overseas. More information can be option from
+                            https://immi.homeaffairs.gov.au/</p>
+                    </div>
+                    <div class="how-to-step">
+                        <h4 class="step-number">Step 6</h4>
+                        <figure class="how-to-step-img"><img src="./assets/img/icon/departure-and-orientation.png" alt=""></figure>
+                        <h3 class="how-to-step-title">Departure and Orientation </h3>
+                        <p class="how-to-step-description">Once the student receives the visa they will need to plan their departure for Australia. Upon arrival, student will join the orientation session get officially enrolled into the program</p>
 
+                    </div>
                 </div>
             </div>
         </div>
